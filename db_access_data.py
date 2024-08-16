c1e5597ff8425d03bead89526891e5998709763b
Tangerine <Butlerlj@ymail.com>
2024-08-16 13:07:57 -0400

    First push in python

---
+++ db_access_data.py
@@ -0,0 +1,19 @@
+# Section 1
+
+# Sys- The sys module in Python provides various functions and variables that are used to manipulate different parts of the Python runtime environment. It allows operating on the interpreter as it provides access to the variables and functions that interact strongly with the interpreter.
+# argv- A list of arguments on the command-line
+
+# Practice 1
+# import sys
+
+# num1 = int(sys.argv[1])
+# num2 = int(sys.argv[2])
+# num3 = int(sys.argv[3])
+# result = (num1 + num2 + num3)
+# sum = result
+# print(sum)
+
+def example(food, place):
+    print(f'{food}, is my favorite food from {place}')
+
+example(chicken, Buds)
